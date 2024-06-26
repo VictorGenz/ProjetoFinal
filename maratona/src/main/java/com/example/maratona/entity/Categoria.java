9943ee5eb3c38734be43e21756618db16284126f
RayanS27 <rayan-vsturmer@educar.rs.gov.br>
2024-06-25 21:09:12 -0300

    Criação das entidades

--- maratona/src/main/java/com/example/maratona/entity/Categoria.java
+++ maratona/src/main/java/com/example/maratona/entity/Categoria.java
@@ -1,14 +1,18 @@
 package com.example.maratona.entity;
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class Categoria {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+
+
+public enum Categoria {
+
+PEQUENO, MEDIO, AVANCADO;
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+private Long id;
 
 }
