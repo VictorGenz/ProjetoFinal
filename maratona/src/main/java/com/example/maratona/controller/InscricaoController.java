acc57068cad48658504ee053ea518e9c462f7da4
VictorGenz <victor-ygenz@educar.rs.gov.br>
2024-06-25 21:09:09 -0300

    Teste

--- maratona/src/main/java/com/example/maratona/controller/InscricaoController.java
+++ maratona/src/main/java/com/example/maratona/controller/InscricaoController.java
@@ -1,9 +1,22 @@
 package com.example.maratona.controller;
 
+import com.example.maratona.entity.Inscricao;
+import com.example.maratona.service.IncricaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("Inscricao")
 public class InscricaoController {
+    @Autowired
+    IncricaoService incricaoService;
+
+    @PostMapping("/cadastrar")
+    public Inscricao cadastrarInscricao(@RequestBody Inscricao inscricao){
+        return incricaoService.salvarInscricao(inscricao);
+    }
+
 }
--- maratona/src/main/java/com/example/maratona/controller/MaratonaController.java
+++ maratona/src/main/java/com/example/maratona/controller/MaratonaController.java
@@ -1,9 +1,21 @@
 package com.example.maratona.controller;
 
+import com.example.maratona.entity.Maratona;
+import com.example.maratona.service.MaratonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("Maratona")
 public class MaratonaController {
+    @Autowired
+    MaratonaService maratonaService;
+
+    @PostMapping("/cadastrar")
+    public Maratona cadastrarMaratona(@RequestBody Maratona maratona){
+        return maratonaService.salvarMaratona(maratona);
+    }
 }
